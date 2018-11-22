48dcd04654e7ede9a325667bad2fb28bd833380a
Mohab999 <mohab@gmail.com>
2018-11-22 19:24:41 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -7,10 +7,10 @@ client.on('ready', () => {
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "515215255719378944"; // ايدي السررفر
+var channel = "515215507000000513";//ايدي الروم
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , BOX , **')
     },305);
 })
 
